7cc21ab668abda33b8915fd46c02460151856358
AlpineIce <cholgate322@gmail.com>
2024-11-28 19:19:18 -0500

    Aquire some vertex data

--- packager/package.py
+++ packager/package.py
@@ -1,4 +1,5 @@
 import bpy
+import mathutils
 
 #----------UI PANEL----------#
 
@@ -23,6 +24,30 @@ class PAPER_ASSET_PACKAGER_PT_package_main_panel(bpy.types.Panel):
         row.scale_y = 2.0
         row.operator("paper.package_operator")
 
+#----------MODEL DATA STRUCTURES----------#
+
+#vertex data
+class Vertex:
+    position = [0, 0, 0]
+    normal = [0, 0, 1]
+    uv = [0, 0]
+
+
+#Model -> LODs -> Meshes
+class Mesh:
+    vbo_offset = -1
+    ibo_offset = -1
+    mat_slot = -1
+
+
+class LOD:
+    meshes = []
+
+
+class ModelData:
+    lod_data = []
+    vertex_data = []
+
 #----------PACKAGE OPERATOR----------#
 
 class PAPER_ASSET_PACKAGER_OT_package_operator(bpy.types.Operator):
@@ -33,28 +58,71 @@ class PAPER_ASSET_PACKAGER_OT_package_operator(bpy.types.Operator):
     def poll(cls, context):
         return context.scene is not None
     
-    def get_base_models(self, base_model_collection):
+    def get_base_models(self, base_model_collection) -> ModelData:
         #return data
-        model_data = {}
+        model_datas = []
         
         #iterate objects in scene
         for object in base_model_collection.all_objects:
             #verify object is mesh type
             if object.type == "MESH":
+                model_data = ModelData()
+
+                #                                                           #
+                #----------TODO HANDLE SMOOTH AND FLAT SHADING!!!!----------#
+                #                                                           #
+
+                #add vertex data to model_data
+                for mesh_vertex in object.data.vertices:
+                    vertex = Vertex()
+                    vertex.position = mesh_vertex.co
+                    vertex.normal = mesh_vertex.normal
+
+                    #vertex.uv = mesh_vertex.
+                    model_data.vertex_data.append(vertex)
+
+                #get UV data
+                uv = object.data.uv_layers.active.uv
+
                 #iterate vertex groups and create map of them first
                 groups = {}
                 for group in object.vertex_groups:
                     #check if group is an LOD
                     if("LOD" in group.name):
                         split = group.name.split("LOD")
-                        groups[int(split[1])] = group.index
+                        groups[int(split[1])] = group
                 
                 #then iterate polygons and associate them with their LOD and material
+                total_index_size = 0
                 for polygon in object.data.polygons:
-                    mat_slot = polygon.material_index
                     indices = polygon.vertices
+                    mat_slot = polygon.material_index
+                    lod_slot = -1
+
+                    total_index_size += len(indices)
+
+                    #find LOD (idk if theres a better way to do this tbh)
+                    for key, value in groups.items():
+                        try:
+                            weight = value.weight(indices[0]) #polygons are guaranteed to have the same LOD for every vertex (there shouldn't be any faces attaching LODs together)
+                            if weight > 0.5:
+                                lod_slot = key
+
+                            break
+                        except:
+                            continue
+
+                    #add model data if an lod slot exists
+                    if lod_slot != -1:
+                        for index in indices:
+                            vertex = object.data.vertices[index]
+
+                print(total_index_size)
+                #model_data.lod_data[lod_slot].meshes[mat_slot]
+
+                    #print(str(lod_slot) + " " + str(mat_slot))
+
                     
-                    print(mat_slot)
                     
     def get_model_instances(self, model_instance_collection):
         for object in model_instance_collection.all_objects:
