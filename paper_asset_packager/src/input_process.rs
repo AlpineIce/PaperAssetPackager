8ba6a82ce65adde6c7bd0b7603f5744f987abbcd
alpineice <cholgate322@gmail.com>
2025-02-02 20:16:11 -0500

    read index data

--- paper_asset_packager/src/input_process.rs
+++ paper_asset_packager/src/input_process.rs
@@ -1,9 +1,10 @@
-use std::fs;
+use std::{fs, ptr};
 
 use gltf;
 
 pub struct MaterialMesh {
-    indices: Vec<f32>,
+    index_stride: u32,
+    index_data: Vec<u8>,
     vertex_stride: u32,
     vertex_data: Vec<u8>
 }
@@ -29,6 +30,20 @@ pub struct ModelData {
     lods: Vec<LOD>
 }
 
+//count, stride
+fn get_vertex_sizing(attributes: gltf::mesh::iter::Attributes) -> (u32, u32, Vec<u32>) {
+    let mut vertex_count: u32 = 0;
+    let mut vertex_stride: u32 = 0;
+    let mut offsets: Vec<u32> = Vec::new();
+    for attribute in attributes {
+        offsets.push(vertex_stride);
+        vertex_stride += attribute.1.data_type().size() as u32;
+        vertex_count = std::cmp::max(vertex_count, attribute.1.count() as u32);
+    }
+    
+    (vertex_count, vertex_stride, offsets)
+}
+
 pub fn process_glb(file_dir: &std::path::PathBuf) -> Option<ModelData> {
     //process gltf
     let gltf_data = match gltf::import(file_dir) {
@@ -36,13 +51,108 @@ pub fn process_glb(file_dir: &std::path::PathBuf) -> Option<ModelData> {
         Err(_err) => return None
     };
 
-    //iterate meshes (LODs in this context)
-    for mesh in gltf_data.0.meshes() {
-        //get mesh name
-        let mesh_name = match mesh.name() {
+    //get buffer data pointer
+    let data_ptr = gltf_data.1[0].as_ptr();
+
+    //iterate scenes
+    for scene in gltf_data.0.scenes() {
+        //use scene name as model name
+        let model_name: String = match scene.name() {
             Some(v) => v,
             None => "Untitled"
-        };
+        }.to_string();
+
+        //print scene name
+        println!("Input processing: {}", model_name);
+
+        if model_name == "Scene" {
+            panic!("Please dont use default scene names");
+        }
+    
+        //iterate nodes (LODs in this context)
+        for node in gltf_data.0.nodes() {
+            //verify node is mesh
+            let mesh = match node.mesh() {
+                Some(v) => v,
+                None => {
+                    println!("  Skipping node that isnt mesh");
+                    continue;
+                }
+            };
+            
+            //print node name
+            match node.name() {
+                Some(v) => println!("  Processing node: {}", v),
+                None => println!("  Processing LOD: Untitled")
+            }
+
+            //create list of meshes
+            let mut meshes: Vec<MaterialMesh> = Vec::new();
+            
+            //iterate primitives
+            for primitive in mesh.primitives() {
+                //get material name
+                let mat_name = match primitive.material().name() {
+                    Some(v) => v,
+                    None => "Untitled"
+                };
+
+                //print material name
+                println!("    Processing LOD mesh on material: {}", mat_name);
+
+                //get index accessor
+                let indices_access = match primitive.indices() {
+                    Some(v) => v,
+                    None => {
+                        println!("  Skipping primitive with no index data accessor");
+                        continue;
+                    }
+                };
+
+                //get buffer view
+                let indices_view = match indices_access.view() {
+                    Some(v) => v,
+                    None => {
+                        println!("  Skipping primitive with no index data view");
+                        continue;
+                    }
+                };
+
+                //get index data
+                let index_stride = indices_access.data_type().size();
+                let index_count = indices_access.count();
+                let indices_buffer_offset = indices_view.offset();
+
+                //create index buffer
+                let mut index_buffer: Vec<u8> = vec![0; index_count * index_stride];
+                let a = index_buffer.as_ptr();
+
+                //copy data into index buffer
+                unsafe { ptr::copy_nonoverlapping(data_ptr.offset(indices_buffer_offset as isize), index_buffer.as_mut_ptr(), index_buffer.len()) };
+
+                //initialize vertex buffer and sizes
+                let (vertex_count, vertex_stride, offsets) = get_vertex_sizing(primitive.attributes());
+                let mut vertex_buffer: Vec<u8> = vec![0; (vertex_count * vertex_stride) as usize];
+
+                //get attributes
+                for index in 0..vertex_count - 1 {
+                    //probaby time to commit this stuff
+                }
+
+                //push back mesh
+                meshes.push(MaterialMesh {
+                    index_stride: index_stride as u32,
+                    index_data: index_buffer,
+                    vertex_stride: vertex_stride,
+                    vertex_data: vertex_buffer
+                });
+            }
+        }
+    }
+
+    //iterate meshes (LODs in this context)
+    for mesh in gltf_data.0.meshes() {
+        
 
         //get primitive data
         for primitive in mesh.primitives() {
@@ -71,9 +181,7 @@ pub fn process_glb(file_dir: &std::path::PathBuf) -> Option<ModelData> {
                 max_z: bounds.max[2]
             };
         }
-
-        let a = 3;
     }
 
-    Some(model_data)
+    None
 }
