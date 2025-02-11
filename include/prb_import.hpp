37683eb296d34015715500cf89ce5a4203d4043e
AlpineIce <cholgate322@gmail.com>
2025-02-11 16:29:04 -0500

    change AABB memory layout

--- include/prb_import.hpp
+++ include/prb_import.hpp
@@ -9,12 +9,12 @@ namespace PaperAssetPackager
 {
     struct AABB
     {
-        float min_x = 0.0f;
         float max_x = 0.0f;
-        float min_y = 0.0f;
+        float min_x = 0.0f;
         float max_y = 0.0f;
-        float min_z = 0.0f;
+        float min_y = 0.0f;
         float max_z = 0.0f;
+        float min_z = 0.0f;
     };
 
     struct MeshData
--- paper_asset_packager/src/common.rs
+++ paper_asset_packager/src/common.rs
@@ -13,12 +13,12 @@ pub struct LOD {
 
 #[repr(C)]
 pub struct AABB {
-    pub min_x: f32,
     pub max_x: f32,
-    pub min_y: f32,
+    pub min_x: f32,
     pub max_y: f32,
-    pub min_z: f32,
-    pub max_z: f32
+    pub min_y: f32,
+    pub max_z: f32,
+    pub min_z: f32
 }
 
 impl AABB {
