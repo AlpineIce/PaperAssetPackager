d9236cf2987b033e15f8bf98a04c250baf2c7aca
AlpineIce <cholgate322@gmail.com>
2025-02-27 17:14:31 -0500

    screen size functionality

--- paper_asset_packager/src/input_process.rs
+++ paper_asset_packager/src/input_process.rs
@@ -1,4 +1,5 @@
-use std::{default, ptr};
+use std::ptr;
+use serde_json::Value;
 
 use super::common;
 
@@ -57,6 +58,52 @@ fn get_vertex_sizing(attributes: gltf::mesh::iter::Attributes, data_ptr: *const 
     })
 }
 
+fn get_screen_size(node: &gltf::Node) -> f64 {
+    //helper variables
+    let default_screen_size = 0.5;
+    let key = "screen_size";
+    let error_msg = format!("| Warning: no LOD screen size selected. Using {} instead.", default_screen_size);
+
+    //skip if on LOD0
+    if node.index() == 0 {
+        return 1.0
+    }
+
+    //check if extras exist
+    match node.extras() {
+        Some(node_val) => {
+            //get json
+            match serde_json::from_str(node_val.get()) {
+                Ok(v) => {
+                    //try to get value from json
+                    let a: serde_json::Map<String, Value> = v;
+                    match a.get(key) {
+                        Some(v) => match v.as_f64() {
+                            Some(v) => v,
+                            None => {
+                                println!("{}", error_msg);
+                                default_screen_size
+                            }
+                        },
+                        None => {
+                            println!("{}", error_msg);
+                            default_screen_size
+                        }
+                    }
+                },
+                Err(_err) => {
+                    println!("{}", error_msg);
+                    default_screen_size
+                }
+            }
+        },
+        None => {
+            println!("{}", error_msg);
+            default_screen_size
+        }
+    }
+}
+
 pub fn process_glb(file_dir: &std::path::PathBuf) -> Option<common::ModelData> {
     //process gltf
     let gltf_data = match gltf::import(file_dir) {
@@ -67,158 +114,164 @@ pub fn process_glb(file_dir: &std::path::PathBuf) -> Option<common::ModelData> {
     //get buffer data pointer
     let data_ptr = gltf_data.1[0].as_ptr();
 
-    //iterate scenes
-    for scene in gltf_data.0.scenes() {
-        //use scene name as model name
-        let model_name: String = match scene.name() {
+    //scene
+    let scene = match gltf_data.0.default_scene() {
+        Some(v) => v,
+        None => return None
+    };
+
+    //use scene name as model name
+    let model_name: String = match scene.name() {
+        Some(v) => v,
+        None => "Untitled"
+    }.to_string();
+
+    //print scene name
+    println!("Input processing: {}", model_name);
+
+    if model_name == "Scene" {
+        panic!("Please dont use default scene names");
+    }
+
+    //initialize AABB
+    let mut aabb = common::AABB {
+        min_x: 0.0,
+        max_x: 0.0,
+        min_y: 0.0,
+        max_y: 0.0,
+        min_z: 0.0,
+        max_z: 0.0
+    };
+    
+    //initialize LOD data vec
+    let mut lods: Vec<common::LOD> = Vec::new();
+    lods.reserve(gltf_data.0.nodes().len());
+
+    //iterate nodes (LODs in this context)
+    for node in gltf_data.0.nodes() {
+        //verify node is mesh
+        let mesh = match node.mesh() {
             Some(v) => v,
-            None => "Untitled"
-        }.to_string();
-
-        //print scene name
-        println!("Input processing: {}", model_name);
-
-        if model_name == "Scene" {
-            panic!("Please dont use default scene names");
-        }
-
-        //initialize AABB
-        let mut aabb = common::AABB {
-            min_x: 0.0,
-            max_x: 0.0,
-            min_y: 0.0,
-            max_y: 0.0,
-            min_z: 0.0,
-            max_z: 0.0
+            None => {
+                println!("  Skipping node that isnt mesh");
+                continue;
+            }
         };
         
-        //initialize LOD data vec
-        let mut lods: Vec<common::LOD> = Vec::new();
-    
-        //iterate nodes (LODs in this context)
-        for node in gltf_data.0.nodes() {
-            //verify node is mesh
-            let mesh = match node.mesh() {
+        //print node (LOD) name
+        match node.name() {
+            Some(v) => println!("| Processing LOD: {}", v),
+            None => println!("  Processing LOD: Untitled")
+        }
+
+        //get screen size
+        let screen_size = get_screen_size(&node);
+
+        //create list of meshes
+        let mut meshes: Vec<common::MaterialMesh> = Vec::new();
+        meshes.reserve(mesh.primitives().len());
+        
+        //iterate primitives
+        for primitive in mesh.primitives() {
+            //get material name
+            let mat_name = match primitive.material().name() {
+                Some(v) => v,
+                None => "Untitled"
+            };
+            let invoke_any_hit = match primitive.material().alpha_mode() {
+                gltf::material::AlphaMode::Opaque => false,
+                _default => true
+            };
+
+            //print material name
+            println!("| | Processing mesh: {}", mat_name);
+            
+            //AABB processing
+            let mesh_bounds = primitive.bounding_box();
+            let mesh_aabb = common::AABB {
+                min_x: mesh_bounds.min[0],
+                max_x: mesh_bounds.max[0],
+                min_y: mesh_bounds.min[1],
+                max_y: mesh_bounds.max[1],
+                min_z: mesh_bounds.min[2],
+                max_z: mesh_bounds.max[2]
+            };
+            aabb.fit_to_max(&mesh_aabb);
+
+            //get index accessor
+            let indices_access = match primitive.indices() {
                 Some(v) => v,
                 None => {
-                    println!("  Skipping node that isnt mesh");
+                    println!("  Skipping primitive with no index data accessor");
                     continue;
                 }
             };
+
+            //get buffer view
+            let indices_view = match indices_access.view() {
+                Some(v) => v,
+                None => {
+                    println!("  Skipping primitive with no index data view");
+                    continue;
+                }
+            };
+
+            //get index data
+            let index_stride = indices_access.data_type().size();
+            let index_count = indices_access.count();
+            let indices_buffer_offset = indices_view.offset();
+
+            //create index buffer
+            let mut index_buffer: Vec<u8> = vec![0; index_count * index_stride];
+
+            //copy data into index buffer
+            unsafe { ptr::copy_nonoverlapping(data_ptr.offset(indices_buffer_offset as isize), index_buffer.as_mut_ptr(), index_buffer.len()) };
+
+            //initialize vertex buffer and sizes
+            let vertex_sizing = match get_vertex_sizing(primitive.attributes(), data_ptr) {
+                Some(v) => v,
+                None => {
+                    println!("  Skipping primitive with no vertex data view");
+                    continue;
+                }
+            };
+            let mut vertex_buffer: Vec<u8> = vec![0; vertex_sizing.get_bytes_size()];
+
+            //let (a, b) = (index_buffer.as_ptr(), vertex_buffer.as_ptr()); //debug helper line
             
-            //print node name
-            match node.name() {
-                Some(v) => println!("  Processing node: {}", v),
-                None => println!("  Processing LOD: Untitled")
-            }
-
-            //create list of meshes
-            let mut meshes: Vec<common::MaterialMesh> = Vec::new();
-            
-            //iterate primitives
-            for primitive in mesh.primitives() {
-                //get material name
-                let mat_name = match primitive.material().name() {
-                    Some(v) => v,
-                    None => "Untitled"
-                };
-                let invoke_any_hit = match primitive.material().alpha_mode() {
-                    gltf::material::AlphaMode::Opaque => false,
-                    _default => true
-                };
-
-                //print material name
-                println!("    Processing LOD mesh on material: {}", mat_name);
-                
-                //AABB processing
-                let mesh_bounds = primitive.bounding_box();
-                let mesh_aabb = common::AABB {
-                    min_x: mesh_bounds.min[0],
-                    max_x: mesh_bounds.max[0],
-                    min_y: mesh_bounds.min[1],
-                    max_y: mesh_bounds.max[1],
-                    min_z: mesh_bounds.min[2],
-                    max_z: mesh_bounds.max[2]
-                };
-                aabb.fit_to_max(&mesh_aabb);
-
-                //get index accessor
-                let indices_access = match primitive.indices() {
-                    Some(v) => v,
-                    None => {
-                        println!("  Skipping primitive with no index data accessor");
-                        continue;
-                    }
-                };
-
-                //get buffer view
-                let indices_view = match indices_access.view() {
-                    Some(v) => v,
-                    None => {
-                        println!("  Skipping primitive with no index data view");
-                        continue;
-                    }
-                };
-
-                //get index data
-                let index_stride = indices_access.data_type().size();
-                let index_count = indices_access.count();
-                let indices_buffer_offset = indices_view.offset();
-
-                //create index buffer
-                let mut index_buffer: Vec<u8> = vec![0; index_count * index_stride];
-
-                //copy data into index buffer
-                unsafe { ptr::copy_nonoverlapping(data_ptr.offset(indices_buffer_offset as isize), index_buffer.as_mut_ptr(), index_buffer.len()) };
-
-                //initialize vertex buffer and sizes
-                let vertex_sizing = match get_vertex_sizing(primitive.attributes(), data_ptr) {
-                    Some(v) => v,
-                    None => {
-                        println!("  Skipping primitive with no vertex data view");
-                        continue;
-                    }
-                };
-                let mut vertex_buffer: Vec<u8> = vec![0; vertex_sizing.get_bytes_size()];
-
-                //let (a, b) = (index_buffer.as_ptr(), vertex_buffer.as_ptr()); //debug helper line
-                
-                //get attributes
-                for i in 0..vertex_sizing.count {
-                    for j in 0..vertex_sizing.offsets.len() {
-                        unsafe {
-                            ptr::copy_nonoverlapping(
-                                vertex_sizing.get_location_ptr(i, j),
-                                vertex_buffer.as_mut_ptr().offset(vertex_sizing.get_indexed_attribute_location(i, j)),
-                                vertex_sizing.attribute_sizes[j]
-                            );
-                        }
+            //get attributes
+            for i in 0..vertex_sizing.count {
+                for j in 0..vertex_sizing.offsets.len() {
+                    unsafe {
+                        ptr::copy_nonoverlapping(
+                            vertex_sizing.get_location_ptr(i, j),
+                            vertex_buffer.as_mut_ptr().offset(vertex_sizing.get_indexed_attribute_location(i, j)),
+                            vertex_sizing.attribute_sizes[j]
+                        );
                     }
                 }
-
-                //push back mesh
-                meshes.push(common::MaterialMesh {
-                    index_stride: index_stride as u32,
-                    index_data: index_buffer,
-                    vertex_stride: vertex_sizing.stride as u32,
-                    vertex_data: vertex_buffer,
-                    invoke_any_hit: invoke_any_hit
-                });
             }
 
-            //add meshes to new LOD
-            lods.push(common::LOD {
-                screen_size: 1.0, //TODO
-                meshes: meshes
+            //push back mesh
+            meshes.push(common::MaterialMesh {
+                index_stride: index_stride as u32,
+                index_data: index_buffer,
+                vertex_stride: vertex_sizing.stride as u32,
+                vertex_data: vertex_buffer,
+                invoke_any_hit: invoke_any_hit
             });
         }
 
-        return Some(common::ModelData {
-            model_name: model_name,
-            bounds: aabb,
-            lods: lods
-        })
+        //add meshes to new LOD
+        lods.push(common::LOD {
+            screen_size: screen_size as f32,
+            meshes: meshes
+        });
     }
 
-    None
+    Some(common::ModelData {
+        model_name: model_name,
+        bounds: aabb,
+        lods: lods
+    })
 }
